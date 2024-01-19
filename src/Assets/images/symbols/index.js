d782020a567d79e281f61998fb7af91faff2b777
CJ484 <ccoreas484@gmail.com>
2024-01-18 23:41:14 -0500

    Add symbol images to index.js

---
+++ src/Assets/images/symbols/index.js
@@ -0,0 +1,12 @@
+import instagram from './instagram.svg';
+import youtube from './youtube.svg';
+import twitter from './twitter.svg';
+import mail from './Mail.svg';
+import phone from './phone.svg';
+import navLeft from './nav_left.svg';
+import navRight from './nav_right.svg';
+import navUp from './nav_up.svg';
+import lightUp from './light_Up.svg';
+import lightDown from './light_Down.svg';
+
+export {instagram, twitter, youtube, mail, phone, navLeft, navRight, navUp, lightUp, lightDown};
