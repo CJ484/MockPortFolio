861535889bcaab3d3d48628e651ed29cf2ef0c3a
CJ484 <ccoreas484@gmail.com>
2024-01-18 23:35:52 -0500

    Add mobile version of contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,38 +1,77 @@
-import { instagram, twitter, youtube, mail, phone } from "../../Assets/images/symbols";
+import {
+  instagram,
+  twitter,
+  youtube,
+  mail,
+  phone,
+} from "../../Assets/images/symbols";
 import smallAvatar from "../../Assets/images/pictures/small_Avatar.jpg";
 import "../../Assets/styles/contact.scss";
 const Contact = () => {
   return (
-    <div className="contactPage">
-      <img className="avatarImg" src={smallAvatar} alt="smallAvatar" />
-      <div>
-        <h1 className="contact_header">Contact</h1>
-        <div className="contact_primary">
+    <>
+      <div className="contactPage">
+        <img className="avatarImg" src={smallAvatar} alt="smallAvatar" />
+        <div>
+          <h1 className="contact_header">Contact</h1>
+          <div className="contact_primary">
             <div className="contact_primary_item">
-                <img src={mail} alt="email" />
-                <p>martanosowska@gmail.com</p>
+              <img src={mail} alt="email" />
+              <p>martanosowska@gmail.com</p>
             </div>
             <div className="contact_primary_item">
-                <img src={phone} alt="phone" />
-                <p>+48 123 123 123</p>
+              <img src={phone} alt="phone" />
+              <p>+48 123 123 123</p>
             </div>
-        </div>
-        <div className="contact_social">
+          </div>
+          <div className="contact_social">
             <div className="contact_social_item">
-                <img src={instagram} alt="instagram"/>
-                <p>@ma.noska</p>
-            </div>
-            <div className="contact_social_item">
-                <img src={twitter} alt="twitter"/>
-                <p>@ma.noska</p>
+              <img src={instagram} alt="instagram" />
+              <p>@ma.noska</p>
             </div>
             <div className="contact_social_item">
-                <img src={youtube} alt="youtube"/>
-                <p>@rysunki_noska</p>
+              <img src={twitter} alt="twitter" />
+              <p>@ma.noska</p>
             </div>
+            <div className="contact_social_item">
+              <img src={youtube} alt="youtube" />
+              <p>@rysunki_noska</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      {/* Mobile version */}
+      <div className="contactPageMobile">
+        <div className="contactMobile_header">
+          <img className="avatarImg" src={smallAvatar} alt="smallAvatar" />
+          <h1 className="contactMobile_title">Contact</h1>
+        </div>
+        <div className="contactMobile_primary">
+          <div className="contactMobile_primary_item">
+            <img src={mail} alt="email" />
+            <p>martanosowska@gmail.com</p>
+          </div>
+          <div className="contactMobile_primary_item">
+            <img src={phone} alt="phone" />
+            <p>+48 123 123 123</p>
+          </div>
+        </div>
+        <div className="contactMobile_social">
+          <div className="contactMobile_social_item">
+            <img src={instagram} alt="instagram" />
+            <p>@ma.noska</p>
+          </div>
+          <div className="contactMobile_social_item">
+            <img src={twitter} alt="twitter" />
+            <p>@ma.noska</p>
+          </div>
+          <div className="contactMobile_social_item">
+            <img src={youtube} alt="youtube" />
+            <p>@rysunki_noska</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
