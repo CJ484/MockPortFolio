c75907ad0ef79f307b37f114a562020c564a8592
CJ484 <ccoreas484@gmail.com>
2024-01-18 19:19:55 -0500

    Update portfolio styles for mobile and web

---
+++ src/pages/Portfolio/index.jsx
@@ -0,0 +1,12 @@
+import { SlideShowWeb, SlideShowMobile } from "../../Components";
+
+const Portfolio = () => {
+  return (
+    <>
+      <SlideShowMobile />
+      <SlideShowWeb />
+    </>
+  );
+};
+
+export default Portfolio;
