e47fced6cd65a29f6cbd26ebfece373942970326
CJ484 <ccoreas484@gmail.com>
2024-01-23 13:49:07 -0500

    Refactor App.js to add theme support and implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
-import {Home, AboutMe, Contact, Portfolio} from './pages';
-import { NavDesktop, NavMobile } from './Components/Nav';
-import './Assets/styles/global.scss';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { Suspense, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home, AboutMe, Contact, Portfolio } from "./pages";
+import { Loading, NavDesktop, NavMobile } from "./Components";
+import "./Assets/styles/global.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-// TODO: Add a navbar
-// TODO: Add a Menu
-// TODO: add react router
+// TODO : Add breadcrumb
+// TODO : add two separate slideshows for the different projects
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const routes = {
+    home: {
+      path: "/",
+      element: <Home theme={theme} />,
+      exact: true,
+    },
+    about: {
+      path: "/about",
+      element: <AboutMe theme={theme}/>,
+      exact: true,
+    },
+    portfolio: {
+      path: "/portfolio",
+      element: <Portfolio theme={theme} />,
+      exact: true,
+    },
+    contact: {
+      path: "/contact",
+      element: <Contact theme={theme} />,
+      exact: true,
+    },
+  };
   return (
     <div className="App">
-      <NavDesktop />
-      {/* <Home /> */}
-      {/* <AboutMe /> */}
-      {/* <Contact /> */}
-      {/* <Portfolio /> */}
+      <NavDesktop theme={theme} setTheme={setTheme}/>
+      <NavMobile theme={theme} setTheme={setTheme} />
+
+      <div className="currentPage">
+        <Routes>
+          {Object.values(routes).map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={
+                <Suspense fallback={<Loading />}>{route.element}</Suspense>
+              }
+              exact={route.exact}
+            />
+          ))}
+        </Routes>
+      </div>
     </div>
   );
 }
