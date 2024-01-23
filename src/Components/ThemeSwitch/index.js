859db22fbe3ac7ce70396feb19852c8046b9d172
CJ484 <ccoreas484@gmail.com>
2024-01-23 13:53:59 -0500

    Refactor ThemeSwitch component to remove unused code and update toggle functionality

--- src/Components/ThemeSwitch/index.js
+++ src/Components/ThemeSwitch/index.js
@@ -1,21 +1,21 @@
 import {Sun, Moon } from '../../Assets/images/symbols';
 import '../../Assets/styles/ThemeSwitch.scss'
-import React, { useState } from 'react';
+import React from 'react';
 import '../../Assets/styles/ThemeSwitch.scss';
-const ThemeSwitch = () => {
-  const [theme, setTheme] = useState('light');
+const ThemeSwitch = ({theme, setTheme}) => {
 
   const toggleTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light');
-    document.querySelector('.themeSwitch').classList.toggle('dark');
+    document.querySelector(".App").classList.toggle('darkMode');
   };
+  
 
   return (
     <div className="themeSwitch">
       <Sun theme={theme}/>
       <div className="switch" onClick={toggleTheme}>
-        <input type="checkbox" id="switch" checked={theme === 'dark'} />
-        <span className="slider round dark"></span>
+        <input type="checkbox" id="switch" checked={theme === 'light'} />
+        <span className="slider round"></span>
       </div>
       <Moon theme={theme} />
     </div>
