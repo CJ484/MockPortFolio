4eabe4bbfdd378b492c779abf2bd409230edfcb9
CJ484 <ccoreas484@gmail.com>
2024-01-22 23:30:30 -0500

    Add mobile menu styles and functionality

--- src/Components/Nav/NavMobile/index.jsx
+++ src/Components/Nav/NavMobile/index.jsx
@@ -1,13 +1,96 @@
+import { Link } from "react-router-dom";
+import ThemeSwitch from "../../ThemeSwitch";
+import { twitter, instagram, youtube } from "../../../Assets/images/symbols";
+import { menuClose, menuOpen } from "../../../Assets/images/symbols";
+import "../../../Assets/styles/mobileMenu.scss";
+
 const NavMobile = () => {
+  const activeMenuItem = () => {
+    const navItems = document.querySelectorAll(
+      ".route_item"
+    );
+    navItems.forEach((item) => {
+      item.addEventListener("click", () => {
+        navItems.forEach((item) => item.classList.remove("active"));
+        item.classList.add("active");
+      });
+    });
+    document.querySelector(".overlay-content").classList.remove("active");
+  };
+
+  const toggleMenu = () => {
+    const overlay = document.querySelector(".overlay-content");
+    overlay.classList.toggle("active");
+  };
+
+  const toggleSubMenu = () => {
+    const subMenu = document.querySelector(".overlay-content__subMenu");
+    subMenu.classList.toggle("active");
+    document
+      .querySelector(".overlay-content__menu_item:nth-child(4)")
+      .classList.toggle("topUnderLine");
+  };
+
   return (
-    <div className="navMobile">
-      <div className="navMobile__menu">
-        <div className="navMobile__menu--item">Home</div>
-        <div className="navMobile__menu--item">About Me</div>
-        <div className="navMobile__menu--item">Portfolio</div>
-        <div className="navMobile__menu--item">Contact</div>
+    <>
+      <div className="overlay-content">
+        <div className="overlay-content_header">
+          <ThemeSwitch />
+          <img onClick={toggleMenu} src={menuOpen} alt="menu" />
+        </div>
+        <div className="overlay-content__menu">
+          <h1 className="overlay-content_title">Menu</h1>
+          <div className="overlay-content_nav">
+            <div className="overlay-content__menu_item route_item">
+              <Link onClick={activeMenuItem} to="/about">
+                About
+              </Link>
+            </div>
+            <div className="overlay-content__menu_item">
+              <h1 onClick={toggleSubMenu}>Portfolio</h1>
+            </div>
+            <div className="overlay-content__subMenu">
+              <div className="overlay-content__subMenu_item route_item">
+                <Link onClick={activeMenuItem} to="/portfolio">
+                  Dwie Siostry
+                </Link>
+              </div>
+              <div className="overlay-content__subMenu_item route_item">
+                <Link onClick={activeMenuItem} to="/portfolio">
+                  Prezkroj
+                </Link>
+              </div>
+            </div>
+            <div className="overlay-content__menu_item route_item">
+              <Link onClick={activeMenuItem} to="/contact">
+                Contact
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="overlay-content__social">
+          <img
+            className="overlay-content__social_item"
+            src={instagram}
+            alt="instagram logo"
+          />
+          <img
+            className="overlay-content__social_item"
+            src={twitter}
+            alt="twitter logo"
+          />
+          <img
+            className="overlay-content__social_item"
+            src={youtube}
+            alt="youtube logo"
+          />
+        </div>
       </div>
-    </div>
+      <div className="nav_Mobile">
+        <h1>Marta Nosowka.</h1>
+        <img onClick={toggleMenu} src={menuClose} alt="menu" />
+      </div>
+    </>
   );
 };
 
