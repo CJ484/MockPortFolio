dac1b16f87efe18050acf4db1bc315110c8263bb
CJ484 <ccoreas484@gmail.com>
2024-01-23 17:03:09 -0500

    Refactor image imports and add new image arrays

--- src/Assets/images/pictures/illustrationWeb/index.js
+++ src/Assets/images/pictures/illustrationWeb/index.js
@@ -1,13 +1,24 @@
-import portfolio1 from './portfolio_1.png';
-import portfolio2 from './portfolio_2.png';
-import portfolio3 from './portfolio_3.png';
-import portfolio4 from './portfolio_4.png';
-import portfolio5 from './portfolio_5.png';
-import portfolio6 from './portfolio_6.png';
-import portfolio7 from './portfolio_7.png';
-import portfolio8 from './portfolio_8.png';
+import portfolio1 from "./portfolio_1.png";
+import portfolio2 from "./portfolio_2.png";
+import portfolio3 from "./portfolio_3.png";
+import portfolio4 from "./portfolio_4.png";
+import portfolio5 from "./portfolio_5.png";
+import portfolio6 from "./portfolio_6.png";
+import portfolio7 from "./portfolio_7.png";
+import portfolio8 from "./portfolio_8.png";
 
-const portfolioImagesWeb = [portfolio1, portfolio2, portfolio3, portfolio4, portfolio5, portfolio6, portfolio7,
-portfolio8];
+const portfolioImagesWeb = [
+  portfolio1,
+  portfolio2,
+  portfolio3,
+  portfolio4,
+  portfolio5,
+  portfolio6,
+  portfolio7,
+  portfolio8,
+];
 
-export default portfolioImagesWeb;
+const DwieSiostryWeb = [portfolio2, portfolio4, portfolio6, portfolio8];
+const PrzekrojWeb = [portfolio1, portfolio3, portfolio5, portfolio7];
+
+export { portfolioImagesWeb, DwieSiostryWeb, PrzekrojWeb };
