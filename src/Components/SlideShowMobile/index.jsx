808c81a3593d7041dfe9b6e7d58d46262a5dd630
CJ484 <ccoreas484@gmail.com>
2024-01-23 19:43:35 -0500

    Refactor mobile portfolio slideshow styles

--- src/Components/SlideShowMobile/index.jsx
+++ src/Components/SlideShowMobile/index.jsx
@@ -1,29 +1,30 @@
-import {navUp} from '../../Assets/images/symbols/'
-import '../../Assets/styles/pages/portfolioMobile.scss';
-const SlideShowMoile = ({photos}) => {
+import { navUp } from "../../Assets/images/symbols/";
+import "../../Assets/styles/pages/portfolioMobile.scss";
+const SlideShowMoile = ({ photos }) => {
+  const backToTop = () => {
+    document.getElementById("portfolio").scrollIntoView({ behavior: "smooth" });
+  };
 
-    // TODO: add breadcrumbs and add a section for all works
-
-    const backToTop = () => {
-        document.getElementById('portfolio').scrollIntoView({behavior: 'smooth'});
-    }
-
-    return (
-        <div id='portfolio' className="portfolioPageMobile">
-        {photos.map((image, index) => {
-            return (
-                    <img
-                        key={index}
-                        className="slideShowMobile_image"
-                        src={image}
-                        alt={"Mobile Illustration"+ index}
-                    />
-
-            );
-        })}
-        <img className='navButton' src={navUp} alt="Button to scroll top" onClick={backToTop}/>
-        </div>
-    )
-}
+  return (
+    <div id="portfolio" className="portfolioPageMobile">
+      {photos.map((image, index) => {
+        return (
+          <img
+            key={index}
+            className="slideShowMobile_image"
+            src={image}
+            alt={"Mobile Illustration" + index}
+          />
+        );
+      })}
+      <img
+        className="navButton"
+        src={navUp}
+        alt="Button to scroll top"
+        onClick={backToTop}
+      />
+    </div>
+  );
+};
 
 export default SlideShowMoile;
