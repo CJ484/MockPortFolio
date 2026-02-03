17256ada637490963091be9078ccf015db4e520a
Carlos Coreas <ccoreas484@gmail.com>
2026-02-02 21:46:32 -0500

    Consolidate image assets by removing individual image imports and creating a centralized image array file for both mobile and web illustrations.

---
+++ src/const/imageArrays.ts
@@ -0,0 +1,58 @@
+// Desktop images
+const portfolioImagesWeb: string[] = [
+  "/images/illustrationWeb/portfolio_1.png",
+  "/images/illustrationWeb/portfolio_2.png",
+  "/images/illustrationWeb/portfolio_3.png",
+  "/images/illustrationWeb/portfolio_4.png",
+  "/images/illustrationWeb/portfolio_5.png",
+  "/images/illustrationWeb/portfolio_6.png",
+  "/images/illustrationWeb/portfolio_7.png",
+  "/images/illustrationWeb/portfolio_8.png",
+];
+
+const DwieSiostryWeb: string[] = [
+  "/images/illustrationWeb/portfolio_2.png",
+  "/images/illustrationWeb/portfolio_4.png",
+  "/images/illustrationWeb/portfolio_6.png",
+  "/images/illustrationWeb/portfolio_8.png",
+];
+const PrzekrojWeb: string[] = [
+  "/images/illustrationWeb/portfolio_1.png",
+  "/images/illustrationWeb/portfolio_3.png",
+  "/images/illustrationWeb/portfolio_5.png",
+  "/images/illustrationWeb/portfolio_7.png",
+];
+
+// Mobile images
+const portfolioImagesMobile: string[] = [
+  "/images/illustrationMobile/1.png",
+  "/images/illustrationMobile/2.png",
+  "/images/illustrationMobile/3.png",
+  "/images/illustrationMobile/4.png",
+  "/images/illustrationMobile/5.png",
+  "/images/illustrationMobile/6.png",
+  "/images/illustrationMobile/7.png",
+  "/images/illustrationMobile/8.png",
+];
+
+const DwieSiostryMobile: string[] = [
+  "/images/illustrationMobile/1.png",
+  "/images/illustrationMobile/2.png",
+  "/images/illustrationMobile/6.png",
+  "/images/illustrationMobile/8.png",
+];
+const PrzekrojMobile: string[] = [
+  "/images/illustrationMobile/3.png",
+  "/images/illustrationMobile/4.png",
+  "/images/illustrationMobile/5.png",
+  "/images/illustrationMobile/7.png",
+];
+
+export {
+  portfolioImagesWeb,
+  DwieSiostryWeb,
+  PrzekrojWeb,
+  portfolioImagesMobile,
+  DwieSiostryMobile,
+  PrzekrojMobile,
+};
